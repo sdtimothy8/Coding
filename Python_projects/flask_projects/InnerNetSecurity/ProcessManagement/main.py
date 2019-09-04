203f17461d7fb657ad50839cb30ba98bbc9db408
sdtimothy8 <sdtimothy8@gmail.com>
2019-09-04 09:44:26 +0800

    delete some python files

--- Python_projects/flask_projects/InnerNetSecurity/ProcessManagement/main.py
+++ Python_projects/flask_projects/InnerNetSecurity/ProcessManagement/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #coding:utf-8
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask import Blueprint, abort
 import os
 import threading
@@ -11,6 +11,7 @@ import util
 
 ProcessManagementRoute = Blueprint('ProcessManagement', __name__)
 process_dict = {}
+process_blacklist = []
 
 # Get all processes
 @ProcessManagementRoute.route('/processes')
@@ -22,26 +23,89 @@ def get_processes():
     return jsonify({'ProcessInfo': result_list})
 
 # Quiery process
-@ProcessManagementRoute.route('/processes/quiery')
-def get_process(process_pid):
-	return jsonify(process_dict)
+@ProcessManagementRoute.route('/processes/quiery', methods=['GET'])
+def quiery_process():
+    """
+    Search the process based on the quiery condition
+    """
+    userName = request.json.get('userName', "")
+    processPid = request.json.get('processPid', "")
+    program = request.json.get('cmd', "")
+    result_list = util.getConditionalProcesses( userName, processPid, program )
+
+    return jsonify({'Conditional process': result_list})
 
 # Close the process
-@ProcessManagementRoute.route('/processes/off')
-def close_process():
-    pass
+@ProcessManagementRoute.route('/processes/off/<string:pid>')
+def close_process(pid):
+    """
+    Close the the process
+    """
+    # Here need consider the important system process
+    cmd = 'kill -9 ' + pid
+    print cmd
+    os.system(cmd)
+    result_list = util.getAllProcesses()
+    return jsonify({'ProcessInfo': result_list})
 
 # Hide some system processes
-@ProcessManagementRoute.route('/processes/hidden')
+@ProcessManagementRoute.route('/processes/hidden', methods=['GET'])
 def hidden_process():
-    pass
+    """
+    Hidden the processes in the process_backlist
+    """
+    process_list = util.getAllProcesses()
+    for black_process in process_blacklist:
+        black_pid = black_process['processPid']
+        task = filter(lambda t: t['pid'] == black_pid, process_list)
+        if len(task) != 0:
+            process_list.remove(task[0])
+    
+    return jsonify({'hidden_process_list': process_list})
 
-# process blacklist management
-@ProcessManagementRoute.route('/processes/operation')
-def process_operation():
-    pass
+
+# Add new process info into process blacklist
+@ProcessManagementRoute.route('/processes/blacklist/add', methods=['POST'])
+def process_blacklist_add():
+    if not request.json or not 'processPid' in request.json:
+        abort(404)
+
+    task = {
+            'processPid': request.json['processPid'],
+            'userName': request.json.get('userName', ""),
+            'cmd': request.json.get('cmd', "")
+            }
+
+    process_blacklist.append(task)
+    #return jsonify({'Result': True}), 201
+    return jsonify({'Result': process_blacklist}), 201
+
+# Delete process info from process blacklist
+@ProcessManagementRoute.route('/processes/blacklist', methods=['GET'])
+def get_process_blacklist():
+    return jsonify({'process_backlist': process_blacklist})
+
+
+# Remove process info from process blacklist
+@ProcessManagementRoute.route('/processes/blacklist', methods=['DELETE'])
+def process_backlist_remove():
+    """
+    Remove the specific processes from the process back list
+    """
+    if not request.json or not 'processPid' in request.json:
+        abort(404)
+
+    pid_list = request.json['processPid']
+    for pid in pid_list:
+        task = filter(lambda t: t['processPid'] == pid, process_blacklist)
+        if len(task) == 0:
+            abort(404)
+        process_blacklist.remove(task[0])
+    
+    return jsonify({'process_backlist': process_blacklist})
+
 
 if __name__ == '__main__':
 	app = Flask(__name__)
 	app.register_blueprint(ProcessManagementRoute, url_prefix='')
-	you app.run(host = '0.0.0.0', port=8080)
+	app.run(host = '0.0.0.0', port=8080)
--- Python_projects/flask_projects/InnerNetSecurity/ProcessManagement/util.py
+++ Python_projects/flask_projects/InnerNetSecurity/ProcessManagement/util.py
@@ -6,8 +6,6 @@ import json
 from string import strip
 import time
 import sys
-import traceback
-import psutil
 # import logging
 # from ksmp import settings
 # from ksmp import logger
@@ -19,34 +17,64 @@ def getAllProcesses():
     """
     result_list = []
     #Todo: flag, node_id = getAgentId()
-    node_id = 1
-    cmd = 'ps -eo pid,vsz,rss,pcpu,pmem,user,time,pri,stat,psr'
+    cmd = 'ps -eo pid,vsz,rss,pcpu,pmem,user,time,pri,stat,psr,comm'
     res = os.popen(cmd).readlines()
     for line in res[1:len(res)]:
         ps_line = line.split()
-        pid = ps_line[0]
-        p = int(pid)
-        try:
-            ps_name = psutil.Process(p).name()
-            ps_dict = {
-                'nodeId': node_id,
-                'pid': pid,
-                'name': ps_name,
-                'vsz': ps_line[1],
-                'rss': ps_line[2],
-                'pcpu': ps_line[3],
-                'pmem': ps_line[4],
-                'user': ps_line[5],
-                'time': ps_line[6],
-                'pri': ps_line[7],
-                'stat': ps_line[8],
-                'psr': ps_line[9]
-            }
-        except psutil.NoSuchProcess:
-            continue
+        ps_dict = {
+            'pid': int(ps_line[0]),
+            'vsz': ps_line[1],
+            'rss': ps_line[2],
+            'pcpu': ps_line[3],
+            'pmem': ps_line[4],
+            'user': ps_line[5],
+            'time': ps_line[6],
+            'pri': ps_line[7],
+            'stat': ps_line[8],
+            'psr': ps_line[9],
+            'name': ps_line[10]
+        }
         result_list.append(ps_dict)
 
     return result_list
+
+def getConditionalProcesses( userName, processPid, program):
+    """
+    return:the process list  include basic information
+    """
+    result_list = []
+    #Todo: flag, node_id = getAgentId()
+    cmd = 'ps -eo pid,vsz,rss,pcpu,pmem,user,time,pri,stat,psr,comm'
+    if len(processPid) != 0:
+        cmd = cmd + " |awk '$1 == %s{print $0}'" %processPid
+    else:
+        if len(userName) != 0:
+            cmd = cmd + " |awk '$6 == \"%s\"{print $0}'" %userName
+
+        if len(program) != 0:
+            cmd = cmd + " | grep %s" %program
+
+    print cmd
+    res = os.popen(cmd).readlines()
+    for line in res:
+        ps_line = line.split()
+        ps_dict = {
+            'pid': int(ps_line[0]),
+            'vsz': ps_line[1],
+            'rss': ps_line[2],
+            'pcpu': ps_line[3],
+            'pmem': ps_line[4],
+            'user': ps_line[5],
+            'time': ps_line[6],
+            'pri': ps_line[7],
+            'stat': ps_line[8],
+            'psr': ps_line[9],
+            'name': ps_line[10]
+        }
+        result_list.append(ps_dict)
+
+    return result_list
+
 
 # if __name__ == "__main__":
 #    flag, ip = getRabbitmqIp()
